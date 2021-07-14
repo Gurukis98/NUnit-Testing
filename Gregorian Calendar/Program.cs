d67ed0e5e6f94eebb55e2287b5044e6c13cdd762
Gurukis98 <84782507+Gurukis98@users.noreply.github.com>
2021-07-14 20:04:38 +0530

    [Guru]Add:NUnit Testing

--- Gregorian Calendar/Program.cs
+++ Gregorian Calendar/Program.cs
@@ -6,26 +6,26 @@ namespace Gregorian_Calendar
     {
        public static void Main(string[] args)
         {
-			DateTime d1 = new DateTime(1998, 10, 30);
-			Console.WriteLine("DOB: " + d1);
+            DateTime date = new DateTime(1998, 10, 30);
+            Console.WriteLine("Date: " + date);
+            Console.WriteLine("Day of a Week");
+            Console.WriteLine(date.ToString("dddd \n"));
 
-			DateTime d2 = new DateTime(2021, 07, 07);
-			Console.WriteLine("Date: " + d2);
+	    Console.WriteLine("Enter The Year");
+            int y = int.Parse(Console.ReadLine());
+       	    Console.WriteLine("Enter The Month");
+	    int m = int.Parse(Console.ReadLine());
+	    Console.WriteLine("Enter The Day");
+	    int d = int.Parse(Console.ReadLine());
 
-			DateTime d3 = new DateTime(2021, 07, 07, 5, 10, 20);
-			Console.WriteLine("Date and Time: " + d3);
-
-			int y = 1998;
-			int m = 10;
-			int d = 30;
             int y0 = (y - (14 - m) / 12);
-            Console.WriteLine("y0 = " + y0);
-			int x = (y0 + y0 / 4 - y0 / 100 + y0 / 400);
+            Console.WriteLine("\ny0 = " + y0);
+	    int x = (y0 + y0 / 4 - y0 / 100 + y0 / 400);
             Console.WriteLine("x = " + x);
-			int m0 = (m + 12 * ((14 - m) / 12) - 2);
+	    int m0 = (m + 12 * ((14 - m) / 12) - 2);
             Console.WriteLine("m0 = " + m0);
-			int d0 = ((d + x + 31 * m0 / 12) % 7);
+	    int d0 = ((d + x + 31 * m0 / 12) % 7);
             Console.WriteLine("d0 = " + d0);
-		}
+	}
     }
 }
