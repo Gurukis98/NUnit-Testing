23e8b5dd8ac6728f8905521df9cb05a66ad9fe0a
Gurukis98 <84782507+Gurukis98@users.noreply.github.com>
2021-07-14 22:34:31 +0530

    [Guru]Add:NUnit Testing

--- Monthly Payment/Program.cs
+++ Monthly Payment/Program.cs
@@ -6,15 +6,16 @@ namespace Monthly_Payment
     {
        public static void Main(string[] args)
         {
+            Console.WriteLine("Enter the Year");
             int Y = int.Parse (Console.ReadLine());
-            int R = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Intrest");
+            double R = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Principal Amount");
             int P = int.Parse(Console.ReadLine());
             int n = 12 * Y;
             double r = R / (12 * 100);
-            var payment = P * ((r / 1 - (Math.Pow(1 + r, -n))));
-            Console.WriteLine("N = " + n);
-            Console.WriteLine("Percent = " + r);
-            Console.WriteLine("Payment = " + payment);
+            double payment = P * r / (1 - Math.Pow(1 + r, -n));
+            Console.WriteLine("Monthly Payment = " + payment);
         }
     }
 }
