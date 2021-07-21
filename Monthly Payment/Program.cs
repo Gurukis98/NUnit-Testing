4da7499cea03714af44607a1f0dcaf72cacddefc
Gurukis98 <84782507+Gurukis98@users.noreply.github.com>
2021-07-21 20:03:50 +0530

    [Guru]Add:Print the EMI Amount

--- Monthly Payment/Program.cs
+++ Monthly Payment/Program.cs
@@ -15,7 +15,7 @@ namespace Monthly_Payment
             int n = 12 * Y;
             double r = R / (12 * 100);
             double payment = P * r / (1 - Math.Pow(1 + r, -n));
-            Console.WriteLine("Monthly Payment = " + payment);
+            Console.WriteLine("Monthly Payment= " + payment);
         }
     }
 }
